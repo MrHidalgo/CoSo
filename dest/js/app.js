c690c851eecb40068d460e7a1ba4932a5066c9d5
MrHidalgo <Luckyhidalgo@gmail.com>
2019-01-21 19:23:57 +0200

    Dashboard: new ticket - create attach section - w\ adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -166,6 +166,65 @@ $(document).ready(function (ev) {
     });
   };
 
+  var initAttachFiles = function initAttachFiles() {
+    var bar = document.getElementById('js-progressbar');
+
+    UIkit.upload('.js-upload', {
+
+      url: '',
+      multiple: true,
+
+      beforeSend: function beforeSend() {
+        console.log('beforeSend', arguments);
+      },
+      beforeAll: function beforeAll() {
+        console.log('beforeAll', arguments);
+      },
+      load: function load() {
+        console.log('load', arguments);
+      },
+      error: function error() {
+        console.log('error', arguments);
+      },
+      complete: function complete() {
+        console.log('complete', arguments);
+      },
+
+      loadStart: function loadStart(e) {
+        console.log('loadStart', arguments);
+
+        bar.removeAttribute('hidden');
+        bar.max = e.total;
+        bar.value = e.loaded;
+      },
+
+      progress: function progress(e) {
+        console.log('progress', arguments);
+
+        bar.max = e.total;
+        bar.value = e.loaded;
+      },
+
+      loadEnd: function loadEnd(e) {
+        console.log('loadEnd', arguments);
+
+        bar.max = e.total;
+        bar.value = e.loaded;
+      },
+
+      completeAll: function completeAll() {
+        console.log('completeAll', arguments);
+
+        setTimeout(function () {
+          bar.setAttribute('hidden', 'hidden');
+        }, 1000);
+
+        alert('Upload Completed');
+      }
+
+    });
+  };
+
   /*
   * CALLBACK :: end
   * ============================================= */
@@ -187,6 +246,7 @@ $(document).ready(function (ev) {
     // ==========================================
     initBodyClick();
     initCollapseSibedar();
+    initAttachFiles();
   };
   initJquery();
 });
